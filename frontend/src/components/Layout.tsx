03be1b08aeae9a79edb619b2af5bb04178755c4a
Daniel Sauble <djsauble@gmail.com>
2025-06-16 12:25:32 -0700

    Add a breadcrumb

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,13 +1,33 @@
-import { Outlet, NavLink } from "react-router";
+import { Outlet, NavLink, useLocation, Link } from "react-router-dom";
+import React from 'react';
 
 export function Layout() {
+  const location = useLocation();
+  const pathnames: string[] = location.pathname.split('/').filter((x: string) => x);
+
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       <div className="bg-white shadow-md">
-        <nav>
-          <NavLink to="/problems" className="block p-4 text-gray-700 hover:bg-gray-200">
+        <nav className="p-4">
+          <Link to="/" className="text-gray-700 hover:underline">
             Design Brain
-          </NavLink>
+          </Link>
+          {pathnames.map((name: string, index: number) => {
+            const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
+            const isLast = index === pathnames.length - 1;
+            return (
+              <React.Fragment key={name}>
+                <span className="mx-2">/</span>
+                {isLast ? (
+                  <span className="text-gray-500">{name.charAt(0).toUpperCase() + name.slice(1)}</span>
+                ) : (
+                  <Link to={routeTo} className="text-gray-700 hover:underline">
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </Link>
+                )}
+              </React.Fragment>
+            );
+          })}
         </nav>
       </div>
       <div className="flex-1 p-8">
